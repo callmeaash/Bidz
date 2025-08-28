09705cecebfba16a16a5ba49988fee634a117deb
callmeaash <aashishbogati9@gmail.com>
2025-08-29 01:08:43 +0545

    add schemas

---
+++ backend/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: int | None = None
+
+
+class RegisterUser(BaseModel):
+    username: str
+    password: str
+    confirm_password: str
+
