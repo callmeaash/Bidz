ecaf69b9da3bef3bd45fee08923fde217119e8a7
callmeaash <aashishbogati9@gmail.com>
2025-08-30 01:37:55 +0545

    add form schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel
+from typing import Optional
+from dataclasses import dataclass
+from fastapi import Form, UploadFile, File
 
 
 class Token(BaseModel):
@@ -12,6 +15,17 @@ class TokenData(BaseModel):
 
 class RegisterUser(BaseModel):
     username: str
+    number: str
     password: str
     confirm_password: str
+
+
+@dataclass
+class ItemForm:
+    title: str = Form(...)
+    description: str = Form(...)
+    category: str = Form(...)
+    starting_bid: float = Form(...)
+    days: int = Form(...)
+    image: Optional[UploadFile] = File(None)
 
